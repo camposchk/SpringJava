63f46283a931348ff34482d1565ba66e55d21f08
Juan Campos <juancampos.chk@gmail.com>
2023-10-26 11:05:50 -0300

    Inicialização do projeto

---
+++ src/main/java/com/senai/javaapi/userController.java
@@ -0,0 +1,5 @@
+package com.senai.javaapi;
+
+public class userController {
+    
+}
---
+++ src/test/java/com/senai/javaapi/JavaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.javaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
